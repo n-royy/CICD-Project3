d79fc9efd0b62afbb907557f2e6407a3b3822689
n-royy <roynguyen18496@gmail.com>
2023-09-24 14:42:35 +0700

    add config

---
+++ .circleci/bash/IPAddress.sh
@@ -0,0 +1,5 @@
+STACK_NAME=$1
+
+INSTACNE_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[0].OutputValue" --output text)
+
+aws ec2 describe-instances --instance-ids $INSTACNE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text
