17c3d37e05ee3f664977ae91d6d83cfa5201ad65
n-royy <roynguyen18496@gmail.com>
2023-09-28 00:39:11 +0700

    add smoke test

--- .circleci/bash/BESmokeTest.sh
+++ .circleci/bash/BESmokeTest.sh
@@ -1,5 +1,5 @@
 # Fetch and prepare the BACKEND_IP env var
-export API_URL="http://${BACKEND_IP}:3030"
+export API_URL="http://107.22.38.187:3030"
 echo "${API_URL}"
 if curl "${API_URL}/api/status" | grep "ok"
 then
