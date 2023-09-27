1c8332698fcdab016686f46d5930acfe7fc25df9
n-royy <roynguyen18496@gmail.com>
2023-09-28 00:36:30 +0700

    add smoke test

--- .circleci/bash/FESmokeTest.sh
+++ .circleci/bash/FESmokeTest.sh
@@ -1,11 +1,9 @@
-ID=$1
-
-URL="http://udapeople-${ID:0:7}.s3-website-us-east-1.amazonaws.com/#/employees"            
+URL="http://udapeople-${CIRCLE_WORKFLOW_ID:0:7}.s3-website-us-east-1.amazonaws.com/#/employees"            
 echo ${URL} 
-if curl -s --connect-timeout 10 ${URL} | grep "Welcome"
+if curl -s ${URL} | grep "Welcome"
 then
 	# Change this to 0 after the job fails
-  exit 1
+  return 1
 else
-  exit 1
+  return 1
 fi
