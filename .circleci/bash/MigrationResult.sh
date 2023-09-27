1b0d4a41ce17fb5c9503721105e637ab52d705ac
n-royy <roynguyen18496@gmail.com>
2023-09-27 20:10:42 +0700

    update config

--- .circleci/bash/MigrationResult.sh
+++ .circleci/bash/MigrationResult.sh
@@ -1,9 +1,9 @@
 if grep -E -i "(has been executed successfully.|No migrations are pending)" ~/project/backend/migrations_dump.txt
 then
      # If you are using kvdb.io, generate the bucket ID "9GE4jRtKznmVKRfvdBABBe" in your local terminal first
-    curl https://kvdb.io/UR8ueHqqqDZZjy8sb2uHYm/migration_${CIRCLE_WORKFLOW_ID:0:7}  -d '1'
+    curl https://kvdb.io/UTtvEbhzE2UwUcFpTFRc5R/migration_${CIRCLE_WORKFLOW_ID:0:7}  -d '1'
     echo "SUCCESS and send to 1"
 else 
-    curl https://kvdb.io/UR8ueHqqqDZZjy8sb2uHYm/migration_${CIRCLE_WORKFLOW_ID:0:7}  -d '0'
+    curl https://kvdb.io/UTtvEbhzE2UwUcFpTFRc5R/migration_${CIRCLE_WORKFLOW_ID:0:7}  -d '0'
     echo "SUCCESS and send to 0"
 fi
